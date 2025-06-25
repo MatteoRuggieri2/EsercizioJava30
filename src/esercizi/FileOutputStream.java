c39ae3272baa28363c0e40b4f8338347119a3665
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-06-25 20:45:44 +0200

    FileOutputStream - Class Scaffolding

--- src/esercizi/FileOutputStream.java
+++ src/esercizi/FileOutputStream.java
@@ -1,10 +1,16 @@
 package esercizi;
 
 public class FileOutputStream {
+	
+	final private String fileName = "src/text_files/file_output_stream_byte.txt";
+	final private String textToWrite = "Questa è una stringa trasformata in un array di bytes.";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new FileOutputStream().run();
+	}
+	
+	private void run() {
+		System.out.println("test");
 	}
 
 }
