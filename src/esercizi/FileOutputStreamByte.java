7da1143acebebe73b6bb35033eaf7c44e6334c07
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-06-26 21:35:52 +0200

    run1() method done

---
+++ src/esercizi/FileOutputStreamByte.java
@@ -0,0 +1,28 @@
+package esercizi;
+
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamByte {
+	
+	final private String fileName = "src/text_files/file_output_stream_byte.txt";
+	final private String textToWrite = "Questa è una stringa trasformata in un array di bytes.";
+
+	public static void main1(String[] args) {
+		FileOutputStreamByte fosb = new FileOutputStreamByte();
+		fosb.run1();
+	}
+	
+	private void run1() {
+		
+		try {
+			OutputStream os1 = new FileOutputStream(fileName);
+			os1.write(textToWrite.getBytes()); // Trasformo il testo in un array di bytes.
+			os1.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
